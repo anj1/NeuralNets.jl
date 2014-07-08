70657442dffdaad1d4f1a78cec4281f399d95be0
zaccranko <zaccranko@192-168-1-38.tpgi.com.au>
2014-07-08 14:18:04 +1000

    moved this to train.jl

--- mlp.jl
+++ mlp.jl
@@ -50,45 +50,4 @@ function calc_offsets(::Type{NNLayer}, dims)
 		offs[i] = sumd
 	end
 	offs
-<<<<<<< HEAD
 end
-=======
-end
-
-# Train a MLP using stochastic gradient decent with momentum.
-# l:        array of neural network layers
-# x:        input data
-# t:        target data
-# η:        learning rate
-# m:        momentum coefficient
-# c:        convergence criterion
-# eval:     how often we evaluate the loss function (20)
-# verbose:  train with printed feedback about the error function (true)
-function gdmtrain{T}(net::Vector{T},x,t,η::Real,c::Real,m::Real=0; eval::Int=20, verbose::Bool=true)
-    i = e_old = Δ_old = 0
-    e_new = loss(prop(net,x),t)
-    in_dim,n = size(x)
-    converged::Bool = false
-    while !converged
-        i += 1
-        ∇,δ = backprop(net,x,t)
-        Δ_new = η*∇ + m*Δ_old  # calculatew Δ weights
-        net = net - Δ_new      # update weights                       
-        Δ_old = Δ_new           
-        if i % eval == 0  # recalculate loss every eval number iterations
-            e_old = e_new
-            e_new = loss(prop(net,x),t)
-            if verbose == true
-                println("i: $i\t Loss=$(round(e_new,6))\t ΔLoss=$(round((e_new - e_old),6))\t Avg. Loss=$(round((e_new/n),6))")
-            end
-        end
-        # check for convergence            
-        abs(e_new - e_old) < c ? converged = true : nothing
-    end
-    println("Training converged in less than $i iterations with average error: $(round((e_new/n),4)).")
-    println("* learning rate η = $η")
-    println("* momentum coefficient m = $m")
-    println("* convergence criterion c = $c")
-    return net
-end
->>>>>>> FETCH_HEAD
