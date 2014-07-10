0960dc53db039dfc80a5f3c36aee757a8941ed19
zaccranko <zaccranko@Zacs-Macbook-Pro.local>
2014-07-10 23:01:25 +1000

    setting up the benchmarks file
    to get some training on real data happening

--- benchmarks.jl
+++ benchmarks.jl
@@ -22,7 +22,14 @@ function dataprep(data)
     return X,T
 end
 
+function untransform(t::Standardize, x::Array)
+    (x .+ t.mean .* t.scale) ./ t.scale
+end
+
 X,T = dataprep(bicycle)
+
+X = X[:,1:100:end] # reduce the size of the data set by a factor of 100
+T = T[:,1:100:end]
 
 trans_x = estimate(Standardize, X; center=true, scale=true)
 trans_t = estimate(Standardize, T; center=true, scale=true)
@@ -30,21 +37,18 @@ trans_t = estimate(Standardize, T; center=true, scale=true)
 transform!(trans_x,X)
 transform!(trans_t,T)
 
-function untransform(t::Standardize, x::Array)
-    (x .+ t.mean .* t.scale) ./ t.scale
-end
-
 ind = size(X,1)
 outd = size(T,1)
 
-layer_sizes = [ind, 3, 3, outd]
-act   = [relu,  relu,  ident]
+layer_sizes = [ind, 10, 10, outd]
+act   = [relud,  relu,  ident]
 actd  = [relud, relud, identd]
 
-# initialize net
-mlp = MLP(randn, layer_sizes, act, actd)
+# not working 100%, it's a difficult set to get to converge in a sensible time period
 
-# training parameters
-params = TrainingParams(100000, 1., 4e-6, 1., :gdmtrain)
+mlp = MLP(rand, layer_sizes, act, actd)
+params = TrainingParams(200e3, 0, 2e-6, .7, :levenberg_marquardt)
+mlp = train(mlp, params, X, T)
 
-gdm_mlp = train(mlp, params, X, T)
+@show untransform(trans_t,T)
+@show untransform(trans_t,prop(mlp,X))
