1f56cce5812bc7c8ee0513f20a81b09c6e594c84
zaccranko <zaccranko@Zacs-Macbook-Pro.local>
2014-07-17 19:55:27 +1000

    renamed to being in line with ForwarDiff naming

--- src/activations.jl
+++ src/activations.jl
@@ -19,7 +19,7 @@ derivs = Dict{Function, Function}([logis => logisd,
 # automatic differentiateion with ForwardDiff.jl
 # due to limitations of ForwardDiff.jl, this function
 # will only produce derivatives with Float64 methods
-function autodif(activ::Function)
+function autodiff(activ::Function)
     f(x) = activ(x[1])
     g(x) = forwarddiff_gradient(f,Float64)([x])[1]
     return g 
--- src/mlp.jl
+++ src/mlp.jl
@@ -28,7 +28,7 @@ function MLP(genf::Function, layer_sizes::Vector{Int}, act::Vector{Function})
         if haskey(derivs,f) 
             push!(actd,derivs[f])
         else
-            push!(actd,autodif(f)) # automatically differentiate f with ForwardDiff.jl   
+            push!(actd,autodiff(f)) # automatically differentiate f with ForwardDiff.jl   
         end
     end
 
