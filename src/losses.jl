048988b263cd4191f12984089f4f12190c7fafa8
pminervini <p.minervini@gmail.com>
2014-07-22 12:27:27 +0200

    update on loss functions

--- src/losses.jl
+++ src/losses.jl
@@ -1,6 +1,29 @@
 # collection of commonly-used loss functions
 
-squared_loss(y, t) = 0.5 * norm(y .- t).^2
-linear_loss(y, t) = norm(y .- t)
-hinge_loss(y, t, m=1.0) = max(0, m - norm(y .* t))
-log_loss(y, t) = - (y .* log(t)) + (1 - y) .* log(1 - t)
+# y = predicted value, t = expected value
+
+squared_loss(y, t) = 0.5 * norm(y .- t).^2 # L(x, z) = .5 || x - z ||^2 = .5 (x - z)^2
+squared_lossd(y, t) = y .- t # d/dx L(x, z) = || x - z ||
+
+linear_loss(y, t) = norm(y .- t, 1) # L(x, z) = || y .- t ||_1 = sum(abs(y .- t))
+linear_lossd(y, t) = sign(y .- t)
+
+hinge_loss(y, t, m=1.0) = sum(max(0, m - (y .* t))) # max(0, m - x * t)
+hinge_lossd(y, t, m=1.0) = -t .* (m > t .* y) # if (m > t x) then - t, else 0
+
+log_loss(y, t) = sum(- ((t .* log(y)) + ((1 - t) .* log(1 - y)))) # - t * log(y) - (1 - t) * log(1 - y)
+log_lossd(y, t) = (t .- y) ./ ((y .- 1) .* y) # t - y / (y - 1) * y
+
+# dictionary of commonly-used loss derivatives
+derivs = Dict{Function, Function}([
+                                   squared_loss      => squared_lossd,
+                                   linear_loss       => linear_lossd,
+                                   hinge_loss        => hinge_lossd,
+                                   log_loss          => log_lossd
+                                   ])
+
+function autodiff(loss::Function)
+    f(x) = loss(x[1])
+    forwarddiff_derivative(x::Float64) = forwarddiff_gradient(f,Float64)([x])[1]
+    return forwarddiff_derivative
+end
