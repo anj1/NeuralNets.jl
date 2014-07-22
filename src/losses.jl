c5bc2a749743161c04e04e131f86a2f1ba2af85c
pminervini <p.minervini@gmail.com>
2014-07-22 12:37:28 +0200

    some fixes

--- src/losses.jl
+++ src/losses.jl
@@ -2,17 +2,17 @@
 
 # y = predicted value, t = expected value
 
-squared_loss(y, t) = 0.5 * norm(y .- t).^2 # L(x, z) = .5 || x - z ||^2 = .5 (x - z)^2
-squared_lossd(y, t) = y .- t # d/dx L(x, z) = || x - z ||
+squared_loss(y, t) = 0.5 * norm(y .- t).^2 # L(y, t) = .5 || y - t ||^2 = .5 (y - t)^2
+squared_lossd(y, t) = y .- t # d/dx L(y, t) = (y - t)
 
-linear_loss(y, t) = norm(y .- t, 1) # L(x, z) = || y .- t ||_1 = sum(abs(y .- t))
+linear_loss(y, t) = norm(y .- t, 1) # L(y, t) = || y .- t ||_1 = sum(abs(y .- t))
 linear_lossd(y, t) = sign(y .- t)
 
-hinge_loss(y, t, m=1.0) = sum(max(0, m - (y .* t))) # max(0, m - x * t)
-hinge_lossd(y, t, m=1.0) = -t .* (m > t .* y) # if (m > t x) then - t, else 0
+hinge_loss(y, t, m=1.0) = sum(max(0, m - (y .* t))) # max(0, m - y t)
+hinge_lossd(y, t, m=1.0) = - t .* (m > t .* y) # if (m > y t) then - t, else 0
 
 log_loss(y, t) = sum(- ((t .* log(y)) + ((1 - t) .* log(1 - y)))) # - t * log(y) - (1 - t) * log(1 - y)
-log_lossd(y, t) = (t .- y) ./ ((y .- 1) .* y) # t - y / (y - 1) * y
+log_lossd(y, t) = (t .- y) ./ ((y .- 1) .* y) # (t - y) / ((y - 1) * y)
 
 # dictionary of commonly-used loss derivatives
 derivs = Dict{Function, Function}([
