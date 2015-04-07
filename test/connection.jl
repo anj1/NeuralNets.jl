2aaf77a0e984c71266b0e92a32953d34638faf6b
Clemente <cuevasclemente@gmail.com>
2015-04-07 18:06:19 -0400

    Add print statments to check tests, added test for DSN connection

--- test/connection.jl
+++ test/connection.jl
@@ -1,6 +1,7 @@
 function test_connection()
     libpq = PostgreSQL.libpq
 
+    println("Testing basic connections")
     conn = connect(Postgres, "localhost", "postgres")
     @test isa(conn, PostgreSQL.PostgresDatabaseHandle)
     @test conn.status == PostgreSQL.CONNECTION_OK
@@ -11,8 +12,10 @@ function test_connection()
     @test bytestring(libpq.PQport(conn.ptr)) == "5432"
     
     disconnect(conn)
+    println("Basic connection function passed")
     @test conn.closed
 
+    println("Trying connection with do block notation")
     conn = connect(Postgres, "localhost", "postgres") do conn
         @test isa(conn, PostgreSQL.PostgresDatabaseHandle)
         @test conn.status == PostgreSQL.CONNECTION_OK
@@ -20,7 +23,19 @@ function test_connection()
         @test !conn.closed
         return conn
     end
+    println("Do block notation passed")
     @test conn.closed
+
+    println("Testing connection with DSN string")
+    conn = connect(Postgres, "postgres://postgres@localhost:5432", "postgres") do conn
+        @test isa(conn, PostgreSQL.PostgresDatabaseHandle)
+        @test conn.status == PostgreSQL.CONNECTION_OK
+        @test errcode(conn) == PostgreSQL.CONNECTION_OK
+        @test !conn.closed
+        return conn
+    end
+    @test conn.closed
+    println("DSN connection passed in a do block")
 end
 
 test_connection()
