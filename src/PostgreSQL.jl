3ca9e1ffc9c9e76ad5cff327901ba1a04447558a
Clemente <cuevasclemente@gmail.com>
2015-04-07 18:02:08 -0400

    Changed name of dbi, exporting more names

--- src/PostgreSQL.jl
+++ src/PostgreSQL.jl
@@ -1,14 +1,16 @@
 module PostgreSQL
     export  Postgres,
             executemany,
-            escapeliteral
+            escapeliteral,
+            execute,
+            prepare
 
     include("libpq.jl")
     using .libpq
     using DBI
     using DataFrames
     using DataArrays
-
     include("types.jl")
-    include("dbi.jl")
+    include("dba.jl")
+
 end
